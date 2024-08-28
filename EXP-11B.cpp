d1e7b206f02436484e609ddea9608ce403b58b0e
Kanwaljeet Singh <97405821+kanwaljeetsingh7887@users.noreply.github.com>
2024-08-28 07:38:01 +0530

    Add files via upload

---
+++ EXP-11B.cpp
@@ -0,0 +1,19 @@
+# include<iostream>
+using namespace std;
+class cuboid
+{
+    public:
+    double h = 2.0,b = 3.0,l = 4.0;
+    double volume()
+    {
+        double vol = h * b * l;
+        cout<<"Volume "<<vol<<endl;
+    }
+
+};
+int main()
+{
+    cuboid c1;
+    c1.volume();
+  
+}
---
+++ EXP-11C.cpp
@@ -0,0 +1,21 @@
+# include<iostream>
+using namespace std;
+class cuboid
+{
+    private:
+    double h = 3.0,b = 2.0,l = 4.0;
+    public:
+    double volume()
+    {
+        double vol;
+        vol = h * b * l;
+        cout<<"Volume "<<vol<<endl;
+    }
+
+};
+int main()
+{
+    cuboid c1;
+    c1.volume();
+  
+}
